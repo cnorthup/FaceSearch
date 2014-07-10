e1af1da0046119979cc1d258f8b86c965d8e7f81
Charles Northup <cjnorth40@gmail.com>
2014-07-10 14:03:51 -0500

    Initial Commit

---
+++ FaceSearch/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  FaceSearch
+//
+//  Created by Charles Northup on 7/10/14.
+//  Copyright (c) 2014 Contract. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ FaceSearch/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  FaceSearch
+//
+//  Created by Charles Northup on 7/10/14.
+//  Copyright (c) 2014 Contract. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
