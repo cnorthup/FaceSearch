a155db1374fa3a2b96525f976b2504070da718ce
Charles Northup <cjnorth40@gmail.com>
2014-07-10 14:58:41 -0500

    caught up

---
+++ FaceSearch/FriendRequestViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FaceSearch
+//
+//  Created by Charles Northup on 7/10/14.
+//  Copyright (c) 2014 Contract. All rights reserved.
+//
+
+#import "FriendRequestViewController.h"
+
+@interface FriendRequestViewController ()
+            
+
+@end
+
+@implementation FriendRequestViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
